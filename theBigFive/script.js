c7af7ba5ebf863955c1c97995d97c4ade13f9efd
BA Dijkman <ba.dijkman@icloud.com>
2021-08-02 18:06:14 +0200

    add opdr the big five

---
+++ theBigFive/script.js
@@ -0,0 +1,42 @@
+let selectBtn = document.getElementsByClassName("big-five-list-item");
+selectBtn = Array.from(selectBtn);
+
+selectBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    onClick(button.innerText);
+  });
+});
+
+const onClick = (innerText) => {
+  const newListItem = document.createElement("li");
+  newListItem.innerText = innerText;
+  newListItem.classList.add("spotted-animals-list-item");
+  const destination = document.getElementById("spotted-animals-list");
+  destination.appendChild(newListItem);
+};
+
+const removeFirstBtn = document.getElementById("remove-first-item-button");
+removeFirstBtn.addEventListener("click", () => {
+  deleteFirst();
+});
+
+const deleteFirst = () => {
+  const destination = document.getElementById("spotted-animals-list");
+  const child = destination.getElementsByTagName("li")[0];
+  let removedItem = destination.removeChild(child);
+};
+
+const removeAllBtn = document.getElementById("remove-all-button");
+removeAllBtn.addEventListener("click", () => {
+  removeAll();
+});
+
+const removeAll = () => {
+  let array = document.getElementsByClassName("spotted-animals-list-item");
+  array = Array.from(array);
+  const destination = document.getElementById("spotted-animals-list");
+  for (i = 0; i < array.length; i++) {
+    const child = destination.getElementsByTagName("li")[0];
+    let removedItem = destination.removeChild(child);
+  }
+};
